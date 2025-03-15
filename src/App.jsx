5eb24bb708cc9f2c9a163f1e0b3bede9070b6838
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-03-15 14:32:09 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className=" text-red-600">hello</h1>
+    </div>
+  );
+}
+
+export default App;
