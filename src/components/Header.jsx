b068b810407b09eb91e83237f7e46d78da5243c1
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-03-18 01:01:17 +0100

    Feat: Make the navbar responsive with a hamburger menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import { IoMenu } from "react-icons/io5";
+import { IoCloseSharp } from "react-icons/io5";
 import Logo from "./Logo";
 import NavLinks from "./NavLinks";
 
 function Header() {
+  const [openNav, setOpenNav] = useState(false);
   return (
     <header className=" flex items-center justify-between px-6 py-4 bg-black">
       <Logo />
-      <NavLinks />
+      <NavLinks openNav={openNav} setOpenNav={setOpenNav} />
+      {openNav ? (
+        <IoCloseSharp
+          onClick={() => setOpenNav((O) => !O)}
+          className=" text-white md:hidden size-8 z-50"
+        />
+      ) : (
+        <IoMenu
+          onClick={() => setOpenNav((O) => !O)}
+          className=" text-white md:hidden size-8 z-50"
+        />
+      )}
     </header>
   );
 }
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Logo() {
-  return <h1 className="logo text-white">GERICHT</h1>;
+  return (
+    <h1 className="logo font-bold text-3xl md:text-[2.7rem] uppercase text-white  z-50">
+      GERICHT
+    </h1>
+  );
 }
 
 export default Logo;
