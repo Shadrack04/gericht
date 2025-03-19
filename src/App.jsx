c9f4ab57843656faaa025a175d4412f08916f2d5
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-03-20 00:09:22 +0100

    Style the mobile view of the about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
 function App() {
+  const [mobileScreen, setMobileScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      const isMobile = window.innerWidth <= 500;
+      setMobileScreen(isMobile);
+    };
+    handleResize();
+    console.log(mobileScreen);
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, [mobileScreen]);
   return (
     <div>
       <Header />
-      <Main />
+      <Main mobileScreen={mobileScreen} />
     </div>
   );
 }
--- src/components/AboutCard.jsx
+++ src/components/AboutCard.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Button from "./Button";
 
-function AboutCard({ title, description, align }) {
-  const style = { textAlign: align };
+function AboutCard({ title, description, align, mobileScreen }) {
+  const style = mobileScreen ? { textAlign: "center" } : { textAlign: align };
   return (
-    <div style={style} className={` md:w-[25rem] h-[18rem] z-30`}>
-      <h2 className="text-[#DCCA87] md:text-[4rem] font-bold">{title}</h2>
-      <p className=" text-white md:text-[1rem]/[175%] font-medium">
+    <div style={style} className={` md:w-[25rem] h-[12rem] md:h-[18rem] z-30`}>
+      <h2 className="text-[#DCCA87] text-[2rem] md:text-[4rem] font-bold">
+        {title}
+      </h2>
+      <p className=" backdrop-blur-sm text-white md:text-[1rem]/[175%] font-medium">
         {description}
       </p>
       <div className=" my-4">
