98eed3e6db5321ba47cc075bbe7b926ef1732905
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-03-21 22:09:31 +0100

    rename logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Logo() {
   return (
     <h1 className="logo font-bold text-3xl md:text-[2.7rem] uppercase text-white  z-50">
-      GERICHT
+      GERICHT Logo
     </h1>
   );
 }
