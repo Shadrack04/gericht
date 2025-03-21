1541ec85f899a9c5035c52f658437fe73327fafb
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-03-21 01:41:16 +0100

    create the special section

--- src/components/Special.jsx
+++ src/components/Special.jsx
@@ -36,7 +36,7 @@ const cocktails = [
 
 function Special() {
   return (
-    <div className=" md:w-[71rem] md:h-[60rem] my-10">
+    <div className=" md:w-[71rem] md:h-[48rem] my-10">
       <div>
         <p className=" text-[1rem] md:text-[1.2rem] text-center text-white">
           Menu that fits you palatte
@@ -47,7 +47,7 @@ function Special() {
       </div>
       <div className=" relative flex flex-col md:flex-row justify-center gap-4 my-10">
         <DrinkList title="Wine & Beer" drinks={wineBeer} />
-        <div className="absolute top-1/2 left-1/2 -translate-1/2 md:translate-0 md:static w-[17rem] h-[31rem] bg-red-500">
+        <div className="absolute top-1/2 left-1/2 -translate-1/2 md:translate-0 md:static w-[17rem] h-[31rem]">
           <img
             src={blender}
             alt="Image of a Blender"
