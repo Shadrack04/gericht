f9481d22a4631eccfd2f30b4983785efa2fb3170
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-03-23 02:19:46 +0100

    Style the hero section

---
+++ src/components/Spoon.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import spoon from "/images/spoon.png";
+
+function Spoon() {
+  return (
+    <div>
+      <img src={spoon} alt="Vector image of a spoon" />
+    </div>
+  );
+}
+
+export default Spoon;
