08649a65c1c10f59943732c9bfed7cd58828f776
Shadrack04 <shadrackjames04@gmail.comgit config --global user.name Shadrack04git config --global user.email shadrackjames04@gmail.com>
2025-03-26 23:27:34 +0100

    Fix: export the laurels details

--- src/components/LaurelsList.jsx
+++ src/components/LaurelsList.jsx
@@ -1,27 +1,5 @@
 import React from "react";
-
-const laurels = [
-  {
-    name: "Bib Gourmond",
-    description: "Lorem ipsum dolor sit amet, consectetur.",
-    image: "/images/num2.png",
-  },
-  {
-    name: "Rising Star",
-    description: "Lorem ipsum dolor sit amet, consectetur.",
-    image: "/images/num1.png",
-  },
-  {
-    name: "AA Hospitality ",
-    description: "Lorem ipsum dolor sit amet, consectetur.",
-    image: "/images/num5.png",
-  },
-  {
-    name: "Outstanding Chef",
-    description: "Lorem ipsum dolor sit amet, consectetur.",
-    image: "/images/num3.png",
-  },
-];
+import { laurels } from "../data/laurels";
 
 export default function LaurelsList() {
   return (
